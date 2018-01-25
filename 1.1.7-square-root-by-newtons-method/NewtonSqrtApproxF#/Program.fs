240b9078f1856f041e7a7dd6e1d74dc0d306fc30
data-pup <kevinmartinucr@gmail.com>
2018-01-24 17:46:31 -0800

    Added implementations for more of the helper functions, and added a scaffold for the recursive function that will be needed.

--- 1.1.7-square-root-by-newtons-method/NewtonSqrtApproxF#/Program.fs
+++ 1.1.7-square-root-by-newtons-method/NewtonSqrtApproxF#/Program.fs
@@ -7,12 +7,24 @@ open System
 let square x = x * x
 
 
+// Find the average of two numbers.
+let average (x, y) = ((x + y) / 2.0)
+
+
+// Improve an approximation.
+let improveGuess (guess : float, x : float) = average ((x/guess), guess)
+
+
 // Check if the square root approximation is within a satisfactory threshold.
 let checkGuess (guess : float, x : float) =
     guess
     |> square
     |> fun i -> x - i
     |> fun i -> i < 0.001
+
+
+let rec sqrtIter(guess : float, x : float) =
+    0
 
 
 [<EntryPoint>]
