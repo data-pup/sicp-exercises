f601774a1fec4a6a4a5a63cb8f770186f29f19f7
data-pup <kevinmartinucr@gmail.com>
2018-01-24 17:35:06 -0800

    Added a function for squaring a value, and for checking the accuracy of a guess.

--- 1.1.7-square-root-by-newtons-method/NewtonSqrtApproxF#/Program.fs
+++ 1.1.7-square-root-by-newtons-method/NewtonSqrtApproxF#/Program.fs
@@ -2,7 +2,21 @@
 
 open System
 
+
+// Find the square of a number x.
+let square x = x * x
+
+
+// Check if the square root approximation is within a satisfactory threshold.
+let checkGuess (guess : float, x : float) =
+    guess
+    |> square
+    |> fun i -> x - i
+    |> fun i -> i < 0.001
+
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+
     0 // return an integer exit code
