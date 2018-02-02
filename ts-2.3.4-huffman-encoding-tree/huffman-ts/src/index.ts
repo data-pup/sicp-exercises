b692acea5cba7be8d01f6811c875d3a732b2bc10
data-pup <kevinmartinucr@gmail.com>
2018-02-01 18:15:45 -0800

    Drafting some more.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -1,5 +1,7 @@
 "use scrict";
 
+import * as stringProcessing from "./lib/stringProcessing";
+
 function main() {
     process.stdout.write("Hello\n");
 }
---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/models/frequencyDictionary.ts
@@ -0,0 +1,5 @@
+
+export const printHello: () => {
+    let returnValue: string = "Hello";
+    return returnValue;
+};
