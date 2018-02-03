868172eaffe92c496ca2595815fe95960148d646
data-pup <kevinmartinucr@gmail.com>
2018-02-03 13:06:10 -0800

    TS is shenanigans.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -5,6 +5,14 @@ import { getCharacterFrequencies } from "./lib/string-processing";
 
 // Define the main function.
 const main = () => {
+
+    // Create a dictionary object, and then add some testing data.
+    const d = new Dictionary<string, number>();
+    d.setValue("hello", 0);
+    d.setValue("world", 1);
+    const dString = d.toString();
+    process.stdout.write(`Dictionary:\n${dString}\n`);
+
     getCharacterFrequencies("Hello!");
 };
 
