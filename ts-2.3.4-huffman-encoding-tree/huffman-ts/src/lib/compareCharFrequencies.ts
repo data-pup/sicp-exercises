d83edde7551bc91420af05705558e985ecac7b54
data-pup <kevinmartinucr@gmail.com>
2018-02-04 11:20:52 -0800

    Combined two variables declarations using array destructuring.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/compareCharFrequencies.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/compareCharFrequencies.ts
@@ -3,8 +3,7 @@ import { ICharFrequency } from "../interfaces/ICharFrequency";
 // Compare CharFrequency objects, this is used to build the priority queue.
 export const compareCharFrequency = (a: ICharFrequency, b: ICharFrequency)
                                         : number => {
-    const aCount = a.getOccurences();
-    const bCount = b.getOccurences();
+    const [aCount, bCount] = [a.getOccurences(), b.getOccurences()];
     if (aCount > bCount) {
         return  1;
     } else if (aCount < bCount) {
