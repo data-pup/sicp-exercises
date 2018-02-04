57d6242caf05f4c05756d16ce819e21b8b2779da
data-pup <kevinmartinucr@gmail.com>
2018-02-04 10:45:05 -0800

    Added a basic example of the comparison function.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -1,17 +1,39 @@
 import { Dictionary, PriorityQueue } from "typescript-collections";
 import { getCharFrequencies } from "./lib/count-characters";
 
+// Define a class that will store the number of occurences for a given char.
+class CharacterFrequency {
+    public character: string;
+    public occurences: number;
+    constructor(c: string, f: number) {
+        this.character = c;
+        this.occurences = f;
+    }
+}
+
+const CompareCharFrequency = (a: CharacterFrequency, b: CharacterFrequency)
+                                : number => {
+    if (a.occurences > b.occurences) {
+        return  1;
+    } else if (a.occurences < b.occurences) {
+        return -1;
+    } else {
+        return  0;
+    }
+};
+
 // Temporary function.
 const tempFunction = (): void => {
-    // Initialize a testing queue.
-    const pq = new PriorityQueue<string>();
-    pq.add("hello");
-    pq.add("hello");
-    pq.add("world");
+    // Initialize a testing queue and add some basic example values.
+    const pq = new PriorityQueue<CharacterFrequency>(CompareCharFrequency);
+    pq.add(new CharacterFrequency("a", 1));
+    pq.add(new CharacterFrequency("b", 2));
+    pq.add(new CharacterFrequency("c", 3));
 
     // Print information about the queue and print the elements.
     process.stdout.write(`Queue has ${pq.size()} elements`);
-    process.stdout.write(`Top queue item is ${pq.peek()}`);
+    process.stdout.write(`Top queue item is ${pq.peek().character}`);
+
 };
 
 // Define a function that will print a dictionary.
