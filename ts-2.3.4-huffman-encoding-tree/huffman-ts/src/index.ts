bb20cf5f8b9478a9dd4a99c18e62b0fb4de55ba9
data-pup <kevinmartinucr@gmail.com>
2018-02-05 12:30:51 -0800

    Priority queue is now being built. Time to start working on building an encoding tree.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -3,10 +3,13 @@ import { CharFreqRecord } from "./classes/CharFreqRecord";
 import { ICharFreqRecord } from "./interfaces/ICharFreqRecord";
 import { compareCharFreqRecords } from "./lib/compareCharFreqRecords";
 import { initializeQueue } from "./lib/initCharFreqQueue";
+import { printCharFreqQueue } from "./lib/printCharFreqQueue";
 
 // Define the main function.
 const main = () => {
-    process.stdout.write("Under construction...\n");
+    const s: string = "testing string";
+    const q = initializeQueue(s);
+    printCharFreqQueue(q);
 };
 
 // Invoke the main function.
--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/printCharFreqQueue.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/printCharFreqQueue.ts
@@ -1,4 +1,14 @@
 import { PriorityQueue } from "typescript-collections";
 import { ICharFreqRecord } from "../interfaces/ICharFreqRecord";
 
-// TODO
+const printICharFreqRecord = (record: ICharFreqRecord): void => {
+    const char = record.getCharacter();
+    const count = record.getOccurences();
+    process.stdout.write(`Character: '${char}'\t# of Occurences: ${count}\n`);
+};
+
+export const printCharFreqQueue = (queue: PriorityQueue<ICharFreqRecord>)
+                                        : void => {
+    process.stdout.write("Queue Contents:\n");
+    queue.forEach(printICharFreqRecord);
+};
