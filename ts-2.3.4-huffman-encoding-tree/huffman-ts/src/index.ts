f75506a888031b85b58f97ec634b49dc2b43a809
data-pup <kevinmartinucr@gmail.com>
2018-02-05 12:02:33 -0800

    Added a cleaner implementation of the preparation process required before building the huffman encoding tree.
    Cleaned up index.ts.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -1,43 +1,12 @@
-import { Dictionary, PriorityQueue } from "typescript-collections";
-
+import { PriorityQueue } from "typescript-collections";
 import { CharFreqRecord } from "./classes/CharFrequency";
 import { ICharFrequency } from "./interfaces/ICharFrequency";
 import { compareCharFrequencies } from "./lib/compareCharFrequencies";
-import { getCharFrequencies } from "./lib/countCharacters";
-
-// Temporary function.
-const tempFunction = (): void => {
-    const s: string = "hellooo";
-    const a: string[] = s.split("").sort();
-
-    const pq = new PriorityQueue<CharFreqRecord>(compareCharFrequencies);
-    let currChar: string = a[0];
-    let currCount: number = 0;
-
-    for (const c of a) {
-        if (c === currChar) {
-            currCount++;
-        } else {
-            // Add a new freq record using the existing values.
-            pq.add(new CharFreqRecord(currChar, currCount));
-            [currChar, currCount] = [c, 1]; // Process the current character.
-        }
-    }
-    pq.add(new CharFreqRecord(currChar, currCount));
-
-    process.stdout.write("Queue contents:");
-    while (!pq.isEmpty()) {
-        process.stdout.write(`${pq.dequeue().getCharacter()}`);
-    }
-};
 
 // Define the main function.
 const main = () => {
-    // Create a dictionary object, and then add some testing data.
-    const s = "pet the good dog";
-    const d = getCharFrequencies(s);
+    process.stdout.write("Under construction...\n");
 };
 
 // Invoke the main function.
-// main();
-tempFunction();
+main();
---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/initCharFreqQueue.ts
@@ -0,0 +1,32 @@
+import { PriorityQueue } from "typescript-collections";
+import { isNullOrUndefined } from "util";
+import { CharFreqRecord } from "../classes/CharFrequency";
+import { compareCharFrequencies } from "./compareCharFrequencies";
+
+// Declare a function that will create a new priority queue used to build
+// the Huffman encoding tree.
+export const initializeQueue = (inputString: string)
+                                : PriorityQueue<CharFreqRecord> => {
+    // Initialize a new priority queue, using the imported comparison function.
+    const freqQueue = new PriorityQueue<CharFreqRecord>(compareCharFrequencies);
+    // Check that the input string was not empty, or undefined.
+    if (isNullOrUndefined(inputString) || inputString.length === 0) {
+        return freqQueue;
+    } else { // Split and sort the input string, initialize loop variables.
+        const sortedChars: string[] = inputString.split("").sort();
+        let currChar: string = sortedChars[0];
+        let currCharCount: number = 0;
+        // Iterate through the array of sorted characters.
+        for (const c of sortedChars) {
+            if (c === currChar) { // Increment the count of the current character.
+                currCharCount++;
+            } else { // If a new character is found, add a new frequency record.
+                freqQueue.add(new CharFreqRecord(currChar, currCharCount));
+                currChar = c; // Process the new character, reset the counter.
+                currCharCount = 1;
+            }
+        } // Add the remaining data to the queue after the loop ends.
+        freqQueue.add(new CharFreqRecord(currChar, currCharCount));
+        return freqQueue; // Return the result.
+    }
+};
