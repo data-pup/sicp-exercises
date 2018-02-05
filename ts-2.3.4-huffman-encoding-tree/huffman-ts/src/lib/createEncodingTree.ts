7cf39af65ec4524a540d55e5f2378cea757cff76
data-pup <kevinmartinucr@gmail.com>
2018-02-05 13:05:22 -0800

    Added a module dependency to the encoding tree creation file.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/createEncodingTree.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/createEncodingTree.ts
@@ -1,5 +1,6 @@
+import { PriorityQueue } from "typescript-collections";
 import { ICharFreqRecord } from "../interfaces/ICharFreqRecord";
 
 export const createEncodingTree = (): void => {
     process.stdout.write("Hello encoding function\n");
-}
+};
