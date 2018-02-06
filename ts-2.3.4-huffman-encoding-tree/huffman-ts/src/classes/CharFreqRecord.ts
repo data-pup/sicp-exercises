2c6acad945b1533de06cb5db644c4ea437040af9
data-pup <kevinmartinucr@gmail.com>
2018-02-06 12:20:16 -0800

    Added input validation to the character frequency record class constructor, and completed adding test coverage to the class.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/CharFreqRecord.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/CharFreqRecord.ts
@@ -1,18 +1,49 @@
 import { ICharFreqRecord } from "../interfaces/ICharFreqRecord";
+import { isNullOrUndefined, isUndefined } from "util";
 
 export class CharFreqRecord implements ICharFreqRecord {
+
+    // Validate the parameters given to the constructor. Throws an exception
+    // if the parameters do not meet the expected requirements.
+    private static validateConstructorParams(c: string, f: number): void {
+        if (isNullOrUndefined(c) || c.length === 0 || c.length > 1) {
+            throw "CharFreqRecord.character must be a single character.";
+        } else if (isNaN(f) || isUndefined(f)) {
+            throw "CharFreqRecord.occurences cannot be undefined or NaN.";
+        } else if (f <= 0) {
+            throw "CharFreqRecord.occurences must be greater than 0.";
+        }
+    }
+
+    // Return a boolean representing whether or not the parameters are valid.
+    public static paramsAreValid(c: string, f: number): boolean {
+        try { // Validate the parameters.
+            CharFreqRecord.validateConstructorParams(c, f);
+        } catch (e) { // Return false if the function threw an exception.
+            return false;
+        } // Otherwise, return true.
+        return true;
+    }
+
+    // Private readonly member variables.
     private readonly character: string;
     private readonly occurences: number;
 
+    // Constructor to create a new character frequency record object.
     constructor(c: string, f: number) {
+        // Check that the given parameters pass validation, if the
+        // validation checks passed, assign values to members.
+        CharFreqRecord.validateConstructorParams(c, f);
         this.character = c;
         this.occurences = f;
     }
 
+    // Accessor method to fetch the internal character value.
     public getCharacter() {
         return this.character;
     }
 
+    // Accessor method to fetch the number of occurences for the character.
     public getOccurences() {
         return this.occurences;
     }
--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/charFreqRecord.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/charFreqRecord.test.ts
@@ -6,32 +6,112 @@ import { CharFreqRecord } from "../src/classes/CharFreqRecord";
 
     @test public basicFreqRecordTest() {
         // Create a new record object.
-        const record = new CharFreqRecord("a", 0);
-
+        const record = new CharFreqRecord("a", 1);
         // Check the character getter method.
         const actualChar = record.getCharacter();
         const expectedChar = "a";
         assert.equal(actualChar, expectedChar, "Expected string 'a'.")
-
         // Check the occurence count getter method.
         const actualOccurences = record.getOccurences();
-        const expectedOccurences = 0;
+        const expectedOccurences = 1;
         assert.equal(actualOccurences, expectedOccurences,
             "Expected occurence value of 0.");
     }
 
+    @test public testZeroIsNotAcceptedAsOccurenceValue() {
+        // Define a function that creates a record with an invalid occurence value.
+        function createInvalidCharFreqRecord() {
+            const a = new CharFreqRecord("a", 0);
+        } // Assert that the function above will raise an exception.
+        assert.throws(createInvalidCharFreqRecord,
+            "CharFreqRecord.occurences must be greater than 0.");
+    }
+
+    @test public testNegativeValueIsNotAccepted() {
+        // Define a function that creates a record with an invalid occurence value.
+        function createInvalidCharFreqRecord() {
+            const a = new CharFreqRecord("a", -0.01);
+        } // Assert that the function above will raise an exception.
+        assert.throws(createInvalidCharFreqRecord,
+            "CharFreqRecord.occurences must be greater than 0.");
+    }
+
+    @test public testNaNIsNotAcceptedAsOccurenceValue() {
+        // Define a function that creates a record with an invalid occurence value.
+        function createInvalidCharFreqRecord() {
+            const a = new CharFreqRecord("a", NaN);
+        } // Assert that the function above will raise an exception.
+        assert.throws(createInvalidCharFreqRecord,
+            "CharFreqRecord.occurences cannot be undefined or NaN.");
+    }
+
+    @test public testEmptyStringIsNotAcceptedAsCharValue() {
+        // Define a function that creates a record with an invalid char value.
+        function createInvalidCharFreqRecord() {
+            const a = new CharFreqRecord("", 1);
+        } // Assert that the function above will raise an exception.
+        assert.throws(createInvalidCharFreqRecord,
+            "CharFreqRecord.character must be a single character.");
+    }
+
+    @test public testLongStringIsNotAcceptedAsCharValue() {
+        // Define a function that creates a record with an invalid char value.
+        function createInvalidCharFreqRecord() {
+            const a = new CharFreqRecord("ABC", 1);
+        } // Assert that the function above will raise an exception.
+        assert.throws(createInvalidCharFreqRecord,
+            "CharFreqRecord.character must be a single character.");
+    }
+
     @test public testPrivateStringCannotBeChanged() {
         // Create a new record object.
-        const record = new CharFreqRecord("a", 0);
-
+        const record = new CharFreqRecord("a", 1);
         // Use the getter method to fetch the character value.
         let s = record.getCharacter();
         s = "new string value"; // Reassign the value.
-
         // Check that the method return is not different.
         let actualChar = record.getCharacter();
         let expectedChar = "a";
         assert.equal(actualChar, expectedChar, "Private object was changed.");
+    }
+
+    @test public testParamsAreValidFunctionWithInvalidInputs() {
+        const invalidValues = [ // Define an array of invalid input values.
+            ["a", 0],
+            ["a", -1],
+            ["", 1],
+            ["Hello", 1],
+            [undefined, 1],
+        ];
+        // Use the map function to find the results for each input case.
+        const invalidCheckResults = invalidValues.map( i => {
+            return CharFreqRecord.paramsAreValid(
+                <string> i[0],
+                <number> i[1],
+            );
+        });
+        // Count the number of inputs that were considered valid.
+        const numberOfPassedChecks = invalidCheckResults
+            .filter( (val) => val == true ).length;
+        const expectedNumberOfPassedChecks = 0;
+        assert.equal(numberOfPassedChecks, expectedNumberOfPassedChecks,
+            "None of the given checks should have passed validation.");
+    }
+
+    @test public testParamsAreValidFunctionWithValidInputs() {
+        const validValues = [ // Define an array of valid input values.
+            ["a", 1],
+        ];
+        const validCheckResults = validValues.map( i => {
+            return CharFreqRecord.paramsAreValid(
+                <string> i[0],
+                <number> i[1],
+            );
+        });
+        const numberOfPassedChecks = validCheckResults
+            .filter( (val) => val == true ).length;
+        const expectedNumberOfPassedChecks = validCheckResults.length;
+        assert.equal(numberOfPassedChecks, expectedNumberOfPassedChecks);
     }
 
 }
