f774c5e0dbf99c9dc79f8d76affad996259e4d8b
data-pup <kevinmartinucr@gmail.com>
2018-02-07 19:37:54 -0800

    Cleaned up the parameter validation process, and added some private static readonly variables to the class for error messages.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/CharFreqRecord.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/CharFreqRecord.ts
@@ -3,6 +3,57 @@ import { ICharFreqRecord } from '../interfaces/ICharFreqRecord';
 
 export class CharFreqRecord implements ICharFreqRecord {
 
+    // Define error messages for constructor parameter validation functions.
+    private static readonly charLengthErrorMessage =
+        'CharFreqRecord.character must be a single character.';
+    private static readonly occurenceUndefinedOrNaNErrorMessage =
+        'CharFreqRecord.occurences cannot be undefined or NaN.';
+    private static readonly occurenceInfinityErrorMessage =
+        'CharFreqRecord.occurences must be finite.';
+    private static readonly occurenceNegativeOrZeroErrorMessage =
+        'CharFreqRecord.occurences must be greater than 0.';
+
+    // Validate the parameters given to the constructor. Throws an exception
+    // if the parameters do not meet the expected requirements.
+    private static validateConstructorParams(c:string, f:number) : void {
+        this.validateCharacterParam(c);
+        this.validateOccurenceParam(f);
+        this.validateOccurenceIsFiniteAndWithinBounds(f);
+    }
+
+    // Validate the constructor's string parameter. This should contain a
+    // single character, and should not be null or undefined.
+    private static validateCharacterParam(c:string) : void {
+        if (isNullOrUndefined(c)) { // Throw an error if c is null or undefined.
+            throw new Error(this.charLengthErrorMessage);
+        }
+        if (c.length != 1) { // Throw an error if c is not the correct length.
+            throw new Error(this.charLengthErrorMessage);
+        }
+    }
+
+    // Validate the occurence parameter. Throws an error object if the value
+    // is NaN, undefined, or Inifinity, otherwise returns nothing. (void)
+    private static validateOccurenceParam(o:number) : void {
+        if (isNaN(o)) { // Throw an error if the value is NaN.
+            throw new Error(this.occurenceUndefinedOrNaNErrorMessage);
+        }
+        if (isUndefined(o)) { // Throw an error if the value is undefined.
+            throw new Error(this.occurenceUndefinedOrNaNErrorMessage);
+        }
+    }
+
+    // Validate whether the occurence parameter, check that it is not Infinity
+    // and is within the expected bounds. Throws an Error otherwise.
+    private static validateOccurenceIsFiniteAndWithinBounds(o:number) : void {
+        if (!isFinite(o)) { // Throw an error if the value is Infinity.
+            throw new Error(this.occurenceInfinityErrorMessage);
+        }
+        if (o <= 0) { // Throw an error if the value is out of bounds.
+            throw new Error(this.occurenceNegativeOrZeroErrorMessage);
+        }
+    }
+
     // Return a boolean representing whether or not the parameters are valid.
     public static paramsAreValid(c:string, f:number) : boolean {
         try { // Validate the parameters.
@@ -11,24 +62,6 @@ export class CharFreqRecord implements ICharFreqRecord {
             return false;
         } // Otherwise, return true.
         return true;
-    }
-
-    // Validate the parameters given to the constructor. Throws an exception
-    // if the parameters do not meet the expected requirements.
-    private static validateConstructorParams(c:string, f:number) : void {
-        if (isNullOrUndefined(c) || c.length === 0 || c.length > 1) {
-            throw new Error( // Error message if string is empty or too long.
-                'CharFreqRecord.character must be a single character.');
-        } else if (isNaN(f) || isUndefined(f)) {
-            throw new Error( // Error message if the occurence count is invalid.
-                'CharFreqRecord.occurences cannot be undefined or NaN.');
-        } else if (!isFinite(f)) {
-            throw new Error( // Error message is the occurence count is infinite.
-                'CharFreqRecord.occurences must be finite.');
-        } else if (f <= 0) {
-            throw new Error( // Error message if the occurence is out of bounds.
-                'CharFreqRecord.occurences must be greater than 0.');
-        }
     }
 
     // Private readonly member variables.
