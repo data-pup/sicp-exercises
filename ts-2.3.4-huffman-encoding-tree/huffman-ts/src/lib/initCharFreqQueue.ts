6771af996e1769877ef5bbcba43d371d6fc1dce8
data-pup <kevinmartinucr@gmail.com>
2018-02-09 10:41:02 -0800

    Cleaned up the character frequency queue intializing function. That was previously a little bit too terse.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/initCharFreqQueue.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/initCharFreqQueue.ts
@@ -7,26 +7,37 @@ import { compareCharFreqRecords } from './compareCharFreqRecords';
 // the Huffman encoding tree.
 export const initializeQueue = (inputString:string)
                                 : PriorityQueue<CharFreqRecord> => {
+
     // Initialize a new priority queue, using the imported comparison function.
     const freqQueue = new PriorityQueue<CharFreqRecord>(compareCharFreqRecords);
-    // Check that the input string was not empty, or undefined.
-    if (isNullOrUndefined(inputString) || inputString.length === 0) {
-        return freqQueue; // Return an empty queue if the input string is empty.
-    } else { // Split and sort the input string, initialize loop variables.
-        const sortedChars:string[] = inputString.split('').sort();
-        let currChar:string = sortedChars[0];
-        let currCharCount:number = 0;
-        // Iterate through the array of sorted characters.
-        for (const c of sortedChars) {
-            if (c === currChar) { // Increment the count of the current character.
-                currCharCount++;
-            } else { // If a new character is found, add a new frequency record.
-                freqQueue.add(new CharFreqRecord(currChar, currCharCount));
-                currChar = c; // Process the new character, reset the counter.
-                currCharCount = 1;
-            }
-        } // Add the remaining data to the queue after the loop ends.
-        freqQueue.add(new CharFreqRecord(currChar, currCharCount));
-        return freqQueue; // Return the result.
+
+     // Return an empty queue if the input string is empty.
+    if (isNullOrUndefined(inputString)) {
+        return freqQueue;
     }
+
+    // Return an empty queue if the input is empty.
+    if (inputString.length === 0) {
+        return freqQueue;
+    }
+
+    // Split and sort the input string, initialize loop variables.
+    const sortedChars:string[] = inputString.split('').sort();
+    let currChar:string = sortedChars[0];
+    let currCharCount:number = 0;
+
+    // Iterate through the array of sorted characters.
+    for (const c of sortedChars) {
+        if (c === currChar) { // Increment the count of the current character.
+            currCharCount++;
+        } else { // If a new character is found, add a new frequency record.
+            freqQueue.add(new CharFreqRecord(currChar, currCharCount));
+            currChar = c; // Process the new character, reset the counter.
+            currCharCount = 1;
+        }
+    }
+
+    // Add the remaining data to the queue after the loop ends.
+    freqQueue.add(new CharFreqRecord(currChar, currCharCount));
+    return freqQueue; // Return the result.
 };
