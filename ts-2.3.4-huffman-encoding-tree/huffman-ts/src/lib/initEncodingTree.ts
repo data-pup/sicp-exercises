d5d62098ac356b1d05974e842e0a3d0a95f6ed68
data-pup <kevinmartinucr@gmail.com>
2018-02-09 15:37:28 -0800

    Added some more drafting implementation of the huffman encoding tree process. The next step is to test this.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/initEncodingTree.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/initEncodingTree.ts
@@ -4,7 +4,9 @@ import { ICharFreqRecord } from '../interfaces/ICharFreqRecord';
 
 export const initializeHuffmanEncodingTree =
              (freqQueue:PriorityQueue<ICharFreqRecord>) : HuffmanBTreeNode => {
-    if (freqQueue.isEmpty) { // Return an empty tree if the queue is empty.
+    // First, check if the frequency queue is empty. An empty tree should be
+    // returned if this queue is empty.
+    if (freqQueue.isEmpty) {
         return new HuffmanBTreeNode([], 0);
     }
 
@@ -12,7 +14,12 @@ export const initializeHuffmanEncodingTree =
     const nodeStack = initializeLeafNodeStack(freqQueue);
 
     while (!treeIsReady(nodeStack)) {
-        throw new Error('Not implemented.');
+        // Pop the two nodes with the lowest priority in the node stack.
+        const leftTree:HuffmanBTreeNode = nodeStack.pop();
+        const rightTree:HuffmanBTreeNode = nodeStack.pop();
+        // Merge the trees together under a new parent node.
+        const mergedTree = HuffmanBTreeNode.mergeTrees(leftTree, rightTree);
+        placeMergedNodeIntoNodeStack(nodeStack, mergedTree);
     }
 
     // Pop the remaining node off the top of the stack, and return it. This
@@ -58,4 +65,50 @@ const treeIsReady = (nodeStack:Stack<HuffmanBTreeNode>) : boolean => {
     }
 
     return false; // Return false otherwise.
+};
+
+// This function is used to place a newly merged tree into the node stack so
+// that the node stack remains correctly sorted by cummulative node weight.
+const placeMergedNodeIntoNodeStack = (nodeStack:Stack<HuffmanBTreeNode>,
+                                      newNode:HuffmanBTreeNode) : void => {
+    // Create a new stack to temporarily store the lower priority nodes that
+    // we are going to push off of the node stack.
+    const tempStack = new Stack<HuffmanBTreeNode>();
+
+    // Until the top of the node stack has a lower cumumulative weight than
+    // the new node that we are going to push, pop the next node off of
+    // the stack and push it into the temporary stack.
+    let currNode:HuffmanBTreeNode = undefined;
+    while (!newNodeShouldBePushedOntoStack(nodeStack, newNode)) {
+        currNode = nodeStack.pop();
+        tempStack.push(currNode);
+    }
+
+    // Place the new node into the node stack in its correct place.
+    nodeStack.push(newNode);
+
+    // Push the contents of the temporary stack back into the node stack.
+    let tempNode:HuffmanBTreeNode = undefined; // Loop variable.
+    while (!tempStack.isEmpty()) {
+        tempNode = tempStack.pop(); // Pop a node off of the temporary stack.
+        nodeStack.push(tempNode); // Push it back onto the node stack.
+    }
+};
+
+const newNodeShouldBePushedOntoStack = (nodeStack:Stack<HuffmanBTreeNode>,
+                                        newNode:HuffmanBTreeNode) : boolean => {
+    // New node should be pushed onto the stack if no nodes remain on the stack.
+    if (nodeStack.isEmpty()) {
+        return true;
+    }
+
+    // New node should be pushed onto the stack if the current top has a
+    // cummulative weight greater than or equal to the new node's weight.
+    if (nodeStack.peek().nodeWeight >= newNode.nodeWeight) {
+        return true;
+    }
+
+    // We should continue to pop nodes off of the stack if neither condition
+    // above was met. This means the new node should be deeper in the stack.
+    return false;
 };
