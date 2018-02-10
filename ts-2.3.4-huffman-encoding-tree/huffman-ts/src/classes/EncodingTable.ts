f0dae5b2e624e5d94f8f6f5dedbc48661fcca1ab
data-pup <kevinmartinucr@gmail.com>
2018-02-10 10:16:15 -0800

    Added a method to the interface, and added a scaffold to the class, that will throw a not implemented error when it is invoked.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/EncodingTable.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/EncodingTable.ts
@@ -6,6 +6,10 @@ export class EncodingTable implements IEncodingTable {
 
     public data:Dictionary<string, string>;
 
+    public encode(input:string) : string {
+        throw new Error('Not Implemented.');
+    }
+
     constructor(tree:IHuffmanBTreeNode) {
         this.data = new Dictionary<string, string>();
     }
--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/interfaces/IEncodingTable.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/interfaces/IEncodingTable.ts
@@ -2,4 +2,5 @@ import { Dictionary } from 'typescript-collections';
 
 export interface IEncodingTable {
     data:Dictionary<string, string>;
+    encode(input:string) : string;
 }
