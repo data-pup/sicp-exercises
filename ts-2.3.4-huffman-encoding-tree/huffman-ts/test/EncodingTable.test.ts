35f3846326bf52c5f53e9931981dffa6d3d5ca3e
data-pup <kevinmartinucr@gmail.com>
2018-02-10 10:38:36 -0800

    Added further scaffolding and todo notes to the encoding tree test class.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/EncodingTable.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/EncodingTable.test.ts
@@ -1,9 +1,35 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
+// import { initializeQueue } from '../src/lib/initCharFreqQueue';
+// import { initializeHuffmanEncodingTree } from '../src/lib/initEncodingTree';
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestEncodingTable {
-    @test public temporaryTest() {
+    @test public testBasicEncodingTableCreation() {
+        // const testString = 'aaabbc';
+
+        // Input String: 'aaabbc' -> Expected Huffman Encoding Tree:
+        // ---------------------------------------------------------
+        // |                [a, b, c] : 6                          |
+        // |               /             \                         |
+        // |           [a] : 3     [b, c] : 3                      |
+        // |                      /          \                     |
+        // |                 [b] : 2     [c] : 1                   |
+        // ---------------------------------------------------------
+
+        // Initialize a priority queue using the given test string.
+        // todo ...
+
+        // Initialize an encoding tree.
+        // todo ...
+
+        // Initialize an encoding table.
+        // todo ...
+
+        // Assert that the encoding scheme is correct.
+        // todo ...
+
+        // Placeholder assertion. (Linting.)
         assert.equal(1, 1);
     }
 }
