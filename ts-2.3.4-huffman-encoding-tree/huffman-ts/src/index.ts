bbc52f088fcbcdd38b2259b0c6ccf237a98cb492
data-pup <kevinmartinucr@gmail.com>
2018-02-09 16:33:44 -0800

    Added some more test case stuff, the initial pass at checking that encoding trees are being created correctly for a given priority queue. Added some content to the index.ts file so that I can debug things.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -1,11 +1,29 @@
-import { initializeQueue } from './lib/initCharFreqQueue';
-import { printCharFreqQueue } from './lib/printCharFreqQueue';
+import { PriorityQueue } from 'typescript-collections';
+import { CharFreqRecord } from './classes/CharFreqRecord';
+import { HuffmanBTreeNode } from './classes/HuffmanBTreeNode';
+import { compareCharFreqRecords } from './lib/compareCharFreqRecords';
+import { initializeHuffmanEncodingTree } from './lib/initEncodingTree';
+
+// Currently unused imports.
+// import { initializeQueue } from './lib/initCharFreqQueue';
+// import { printCharFreqQueue } from './lib/printCharFreqQueue';
 
 // Define the main function.
 const main = () => {
-    const s = 'This is a testing string, to demonstrate a queue.';
-    const q = initializeQueue(s);
-    printCharFreqQueue(q);
+    const testQueue = new PriorityQueue<CharFreqRecord>(
+        compareCharFreqRecords);
+
+    // Push some character frequency records onto the stack.
+    testQueue.enqueue(new CharFreqRecord('a', 3));
+    testQueue.enqueue(new CharFreqRecord('b', 2));
+    testQueue.enqueue(new CharFreqRecord('c', 1));
+
+    // Create an encoding tree using the testing priority queue.
+    const encodingTree:HuffmanBTreeNode = initializeHuffmanEncodingTree(
+        testQueue);
+
+    // Print something so that the linter will remain quiet.
+    process.stdout.write(encodingTree.childVals.toString());
 };
 
 // Invoke the main function.
--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/initEncodingTree.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/initEncodingTree.test.ts
@@ -31,4 +31,41 @@ import { initializeHuffmanEncodingTree } from '../src/lib/initEncodingTree';
         assert.isUndefined(encodingTree.left);
         assert.isUndefined(encodingTree.right);
     }
+
+    @test public testEncodingTreeCreationForSimpleQueue() {
+        // Initialize a priority queue to store test data.
+        const testQueue = new PriorityQueue<CharFreqRecord>(
+            compareCharFreqRecords);
+
+        // Push some character frequency records onto the stack.
+        testQueue.enqueue(new CharFreqRecord('a', 3));
+        testQueue.enqueue(new CharFreqRecord('b', 2));
+        testQueue.enqueue(new CharFreqRecord('c', 1));
+
+        // Create an encoding tree using the testing priority queue.
+        const encodingTree:HuffmanBTreeNode = initializeHuffmanEncodingTree(
+            testQueue);
+
+        // Expected encoding tree structure:
+        // | -------------------------------------- |
+        // |          [a, b, c] : 6                 |
+        // |         /             \                |
+        // |    [a] : 3          [b, c] : 3         |
+        // |                    /          \        |
+        // |                [b] : 2    [c] : 1      |
+        // |                                        |
+        // | -------------------------------------- |
+
+        // Check the generated encoding tree matches the above structure:
+        // --------------------------------------------------------------
+
+        // Check that the root node contains [a, b, c] and has weight 6.
+        const expectedRootWeight = 6;
+        assert.equal(encodingTree.nodeWeight, expectedRootWeight);
+        const expectedRootChildList = ['a', 'b', 'c'];
+        assertArraysAreEqual(encodingTree.childVals, expectedRootChildList);
+
+        // ...
+    }
+
 }
