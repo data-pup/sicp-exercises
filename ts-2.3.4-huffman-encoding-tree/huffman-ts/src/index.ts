f1f0ce12b9503f0b9f3767d9e0c1949473c49075
data-pup <kevinmartinucr@gmail.com>
2018-02-10 10:54:37 -0800

    Adjusted the package.json file, moved the serve command into the 'start' script so that the program can be run using `npm start`, which will also kick off the build and lint process. Other content added to the index file to print information about a node.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/index.ts
@@ -1,11 +1,22 @@
-// import { PriorityQueue } from 'typescript-collections';
-// import { CharFreqRecord } from './classes/CharFreqRecord';
 import { HuffmanBTreeNode } from './classes/HuffmanBTreeNode';
 import { initializeQueue } from './lib/initCharFreqQueue';
 import { initializeHuffmanEncodingTree } from './lib/initEncodingTree';
 
+// Unused Imports:
+// ----------------------------------------------------------
+// import { Queue } from 'typescript-collections';
+// import { CharFreqRecord } from './classes/CharFreqRecord';
+// ----------------------------------------------------------
+
 const testing = (data:HuffmanBTreeNode) : void => {
-    process.stdout.write('Hello Testing!');
+    // Print some overview information about the root node.
+    process.stdout.write('Received root node with the following properties:\n');
+    process.stdout.write(`\tWeight: ${data.weight}\n`);
+    process.stdout.write('\tTokens:\n');
+    for (const token of data.tokens) {
+        process.stdout.write(`\t\t${token}\n`);
+    }
+    process.stdout.write('\n');
 };
 
 // Define the main function.
