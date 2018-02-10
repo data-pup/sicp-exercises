3874c6fbf11c510fa7e12d8b2365e1232e8cff67
data-pup <kevinmartinucr@gmail.com>
2018-02-10 10:21:50 -0800

    Added a basic scaffold for the test class for the encoding table.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/EncodingTable.test.ts
@@ -0,0 +1,9 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestEncodingTable {
+    @test public temporaryTest() {
+        assert.equal(1, 1);
+    }
+}
