7f6a67ff22f75a832d0cd5254496788e181fb5c4
data-pup <kevinmartinucr@gmail.com>
2018-02-10 18:58:09 -0800

    Added a scaffold to the checkHuffman.. file. Functions will throw error objects when they are invoked.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/checkHuffmanNodeValidity.ts
@@ -0,0 +1,14 @@
+import { IHuffmanBTreeNode } from '../interfaces/IHuffmanBTreeNode';
+
+const validateLeafNode = () : boolean => {
+    throw new Error('Not Implemented');
+};
+
+const validateParentNode = () : boolean => {
+    throw new Error('Not Implemented');
+};
+
+// Function used to validate a Huffman encoding tree node.
+export const validateIHuffmanBTreeNode = () : boolean => {
+    throw new Error('Not Implemented');
+};
