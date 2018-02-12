8e8e553bc933a592c257cce05aa6c8adeb5878b6
data-pup <kevinmartinucr@gmail.com>
2018-02-11 19:15:01 -0800

    Added placeholder scaffold to the invalid parent node test suite.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/HuffmanBTreeNodeInvalidParentNode.test.ts
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import { HuffmanBTreeNode } from '../src/classes/HuffmanBTreeNode';
+// import { InvalidNodeErrorMessages } from '../src/lib/invalidNodeErrorMessages';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestHuffmanBTreeNodeInvalidLeaf {
+
+    @test public testTemporary() {
+        assert.equal(1, 1, 'Temporary placeholder.');
+    }
+
+}
