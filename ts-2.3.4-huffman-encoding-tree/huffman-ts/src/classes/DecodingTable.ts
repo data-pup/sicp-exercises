37e78183df895429bd5fe0734ac7d8b401c1e8c9
data-pup <kevinmartinucr@gmail.com>
2018-02-12 09:46:11 -0800

    Added scaffolds to the decoding table class implementation and to the interface definition.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/DecodingTable.ts
@@ -0,0 +1,18 @@
+import { Dictionary } from 'typescript-collections';
+import { IDecodingTable } from '../interfaces/IDecodingTable';
+import { IHuffmanBTreeNode } from '../interfaces/IHuffmanBTreeNode';
+
+// Disabled import, factory method to be implemented later.
+// import { IEncodingTable } from '../interfaces/IEncodingTable';
+
+export class DecodingTable implements IDecodingTable {
+    public data:Dictionary<string, string>;
+
+    public decode(input:string) : string {
+        throw new Error('Not Implemented.');
+    }
+
+    constructor(tree:IHuffmanBTreeNode) {
+        this.data = new Dictionary<string, string>();
+    }
+}
