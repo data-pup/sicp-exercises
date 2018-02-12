463fc78b3be87050878bdebf4d469372dc88b966
data-pup <kevinmartinucr@gmail.com>
2018-02-11 17:34:58 -0800

    Added public static definitions of error messages to the invalidNodeErrorMessages class.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/lib/invalidNodeErrorMessages.ts
@@ -0,0 +1,27 @@
+export class InvalidNodeErrorMessages {
+
+    public static leafNodeHasMultipleTokens:string =
+        'Leaf node has multiple tokens!';
+
+    public static leafNodeIsMissingWeight:string =
+        'Leaf node must have a non-zero weight!';
+
+    public static parentNodeIsMissingTokens:string =
+        'Parent node must have a token array!';
+
+    public static parentNodeIsExtraneous:string =
+        'Extraneous parent node detected!';
+
+    public static parentNodeIsMissingWeight:string =
+        'Parent node must have a non-zero weight!';
+
+    public static parentNodeHasUnsortedTokens:string =
+        'Parent node must have a sorted token array!';
+
+    public static parentNodeHasIncorrectWeight:string =
+        'Parent node\'s weight must match the sum of its children!';
+
+    public static parentNodeHasDuplicateTokens:string =
+        'Parent node cannot have duplicate tokens!';
+
+}
