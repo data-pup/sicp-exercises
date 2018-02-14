7504fc0de3faa1903d94cf74bf9d0e0ec813384d
data-pup <kevinmartinucr@gmail.com>
2018-02-13 18:04:26 -0800

    Added a test suite & a test case for checking that an encoding dictionary can be created correctly, and that input strings can be correctly build according to that schema.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/EncodingTable.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/EncodingTable.ts
@@ -8,11 +8,16 @@ export class EncodingTable implements IEncodingTable {
     public readonly encodingScheme:Dictionary<string, string>;
 
     public encode(input:string) : string {
-        throw new Error('Not Implemented.');
+        const inputChars = input.split('');
+        const encodedChars = inputChars.map((char) => {
+            return this.encodingScheme.getValue(char);
+        });
+        const encodedOutput = encodedChars.join();
+        return encodedOutput;
     }
 
     constructor(huffmanTree:IHuffmanBTreeNode) {
-        this.encodingScheme = createEncodingDictionary(huffmanTree, '0');
+        this.encodingScheme = createEncodingDictionary(huffmanTree, '1');
     }
 
 }
--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/EncodingTable.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/EncodingTable.test.ts
@@ -1,13 +1,14 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
-// import { initializeQueue } from '../src/lib/initCharFreqQueue';
-// import { initializeHuffmanEncodingTree } from '../src/lib/initEncodingTree';
+import { Dictionary } from 'typescript-collections';
+import { EncodingTable } from '../src/classes/EncodingTable';
+import { initializeQueue } from '../src/lib/initCharFreqQueue';
+import { initializeHuffmanEncodingTree } from '../src/lib/initEncodingTree';
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestEncodingTable {
-    @test public testBasicEncodingTableCreation() {
-        // const testString = 'aaabbc';
 
+    @test public testBasicEncodingTableCreation() {
         // Input String: 'aaabbc' -> Expected Huffman Encoding Tree:
         // ---------------------------------------------------------
         // |                [a, b, c] : 6                          |
@@ -17,19 +18,51 @@ import { suite, test } from 'mocha-typescript';
         // |                 [b] : 2     [c] : 1                   |
         // ---------------------------------------------------------
 
+        // Expected Encoding Table:
+        // ---------------------------------------------------------
+        // | a -> 10                                               |
+        // | b -> 110                                              |
+        // | c -> 111                                              |
+        // ---------------------------------------------------------
+
+        // Initialize the input string.
+        const testString = 'aaabbc';
+
         // Initialize a priority queue using the given test string.
-        // const freqQueue = initializeQueue(testString);
+        const freqQueue = initializeQueue(testString);
 
         // Initialize an encoding tree.
-        // const encodingTree = initializeHuffmanEncodingTree(freqQueue);
+        const encodingTree = initializeHuffmanEncodingTree(freqQueue);
 
         // Initialize an encoding table.
-        // todo ...
+        const encoder = new EncodingTable(encodingTree);
 
-        // Assert that the encoding scheme is correct.
-        // todo ...
+        // Initialize and fill a dictionary containing characters from
+        // the test string and their respective encodings.
+        const expectedEncodings = new Dictionary<string, string>();
+        expectedEncodings.setValue('a', '10');
+        expectedEncodings.setValue('b', '110');
+        expectedEncodings.setValue('c', '111');
 
-        // Placeholder assertion. (Linting.)
-        assert.equal(1, 1, 'Temporary placeholder.');
+        // Assert the encoding scheme is correct by comparing the expected
+        // and actual encoding for each key in the expected dictionary.
+        expectedEncodings.forEach((char, expectedEncoding) : void => {
+            // Get the character's actual encoding and assert is it correct.
+            const actualEncoding:string = encoder.encode(char);
+            assert.equal(actualEncoding, expectedEncoding);
+        });
+
+        // Manually initialize a string containing the expected encoding
+        // of the test string, using the expected schema defined above.
+        const expectedTestStringEncoding:string = [
+            '10', '10', '10', '110', '110', '111',
+        ].join();
+
+        // Initialize a variable containing the actual test string encoding.
+        const actualTestStringEncoding = encoder.encode(testString);
+
+        // Assert the expected and actual test string encoding match.
+        assert.equal(actualTestStringEncoding, expectedTestStringEncoding);
     }
+
 }
