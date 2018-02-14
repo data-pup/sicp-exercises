e727bd59a5b96094fa34fa7ae624bfb7fc746069
data-pup <kevinmartinucr@gmail.com>
2018-02-13 18:36:57 -0800

    Added a function signature for the function that will be used to generate a decoding schema for the decoding table constructor.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/DecodingTable.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/classes/DecodingTable.ts
@@ -4,6 +4,14 @@ import { IEncodingTable } from '../interfaces/IEncodingTable';
 
 export class DecodingTable implements IDecodingTable {
 
+    // This is a private static helper function used by the constructor
+    // to convert the schema of an encoding dictionary to a decoding
+    // schema. This will return a Dictionary<string, string> object.
+    private static generateDecodingScheme(encoder:IEncodingTable)
+                                         : Dictionary<string, string> {
+        throw new Error('Not Implemented');
+    }
+
     public readonly decodingScheme:Dictionary<string, string>;
 
     public decode(input:string) : string {
@@ -11,6 +19,6 @@ export class DecodingTable implements IDecodingTable {
     }
 
     constructor(encoder:IEncodingTable) {
-        this.decodingScheme = new Dictionary<string, string>();
+        this.decodingScheme = DecodingTable.generateDecodingScheme(encoder);
     }
 }
