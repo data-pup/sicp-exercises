ded271748be0bcdf93944028452fe4b98ae23aef
data-pup <kevinmartinucr@gmail.com>
2018-02-13 18:50:46 -0800

    'Linting errors aren't real' I tell myself as I make commits without building the project first.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/DecodingTable.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/DecodingTable.test.ts
@@ -3,6 +3,7 @@ import { suite, test } from 'mocha-typescript';
 // import { DecodingTable } from '../src/classes/DecodingTable';
 // import { EncodingTable } from '../src/classes/EncodingTable';
 
+/* tslint:disable-next-line:no-unused-variable */
 @suite class TestDecodingTable {
 
     @test public placeholderTest() {
