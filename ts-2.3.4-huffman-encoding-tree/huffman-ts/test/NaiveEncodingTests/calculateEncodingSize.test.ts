024fecadddb83447834233a235abb8a57a156634
data-pup <kevinmartinucr@gmail.com>
2018-02-16 10:26:56 -0800

    Added a test to make sure that an undefined token array will cause the encoding width calculation method to raise a specific error.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/NaiveEncodingTests/calculateEncodingSize.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/NaiveEncodingTests/calculateEncodingSize.test.ts
@@ -1,14 +1,20 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
-// import {
-//     calculateEncodingSize,
-// } from '../../src/NaiveEncoding/calculateEncodingSize';
+import {
+    calculateEncodingSize,
+} from '../../src/NaiveEncoding/calculateEncodingSize';
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestCalculateEncodingSize {
 
-    @test public placeholderTest() {
-        assert.equal(1, 2, 'Tests not implemented yet!');
+    @test public testUndefinedInputRaisesError() {
+        const expectedError:string = 'Error calculating encoding string size!';
+        assert.throws(
+            () => {
+                const testValue:string[] = undefined;
+                calculateEncodingSize(testValue);
+            },
+            expectedError);
     }
 
 }
