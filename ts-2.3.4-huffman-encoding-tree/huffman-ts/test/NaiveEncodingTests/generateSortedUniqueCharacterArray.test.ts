7bdc365cb6037aa4460a7ff025615e9ee3d1a00d
data-pup <kevinmartinucr@gmail.com>
2018-02-15 21:53:42 -0800

    Implemented basic tests for the sorted character array generation logic.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/NaiveEncodingTests/generateSortedUniqueCharacterArray.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/NaiveEncodingTests/generateSortedUniqueCharacterArray.test.ts
@@ -1,14 +1,40 @@
-import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
-// import {
-//     getSortedUniqueCharArray,
-// } from '../../src/NaiveEncoding/generateSortedUniqueCharacterArray';
+import {
+    getSortedUniqueCharArray,
+} from '../../src/NaiveEncoding/generateSortedUniqueCharacterArray';
+import { assertArraysAreEqual } from '../testUtils/assertArraysAreEqual';
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGenerateEncodingString {
 
-    @test public placeholderTest() {
-        assert.equal(1, 2, 'Tests not implemented yet!');
+    @test public testUndefinedValueReturnsEmptyArray() {
+        const inputString:string = undefined;
+        const charArray:string[] = getSortedUniqueCharArray(inputString);
+        const expectedCharArray:string[] = new Array<string>();
+        assertArraysAreEqual(charArray, expectedCharArray);
+    }
+
+    @test public testEmptyValueReturnsEmptyArray() {
+        const inputString:string = '';
+        const charArray:string[] = getSortedUniqueCharArray(inputString);
+        const expectedCharArray:string[] = new Array<string>();
+        assertArraysAreEqual(charArray, expectedCharArray);
+    }
+
+    @test public testHelloWorld() {
+        const inputString:string = 'hello world';
+        const charArray:string[] = getSortedUniqueCharArray(inputString);
+        const expectedCharArray:string[] = [
+            ' ',
+            'd',
+            'e',
+            'h',
+            'l',
+            'o',
+            'r',
+            'w',
+        ];
+        assertArraysAreEqual(charArray, expectedCharArray);
     }
 
 }
