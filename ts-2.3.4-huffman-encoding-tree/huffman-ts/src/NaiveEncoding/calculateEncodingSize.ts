be2bed51346d2b4c6ecd4a1533a2fc5bba6d9915
data-pup <kevinmartinucr@gmail.com>
2018-02-16 10:19:23 -0800

    Added some extra validation to the encoding size calculation method, and added a placeholder for the test suite for this function. For some reason, there are some warnings popping up about the decorators in VS. Not sure if this is just an IDE issue, but everything seems to be building fine at the moment.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/NaiveEncoding/calculateEncodingSize.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/NaiveEncoding/calculateEncodingSize.ts
@@ -1,8 +1,19 @@
+import { isNullOrUndefined } from 'util';
 import { convertNumberToEncodingString } from './generateEncodingString';
 
 // This function will calculate the padding size, by finding the length
 // of the largest encoding value.
 export const calculateEncodingSize = (tokens:string[]) : number => {
+    if (isNullOrUndefined(tokens)) {
+        throw new Error('Error calculating encoding string size!');
+    }
+
+    // If the token array contains 0 or 1 elements, return a length of 1.
+    if (tokens.length <= 1) {
+        return 1;
+    }
+
+    // Calculate the largest encoding value, and return its length.
     const maxEncodingValue:number = tokens.length - 1;
     const encoding:string = convertNumberToEncodingString(maxEncodingValue);
     return encoding.length;
---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/NaiveEncodingTests/calculateEncodingSize.test.ts
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import {
+//     calculateEncodingSize,
+// } from '../../src/NaiveEncoding/calculateEncodingSize';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestCalculateEncodingSize {
+
+    @test public placeholderTest() {
+        assert.equal(1, 2, 'Tests not implemented yet!');
+    }
+
+}
