82c85ef325f1d910eed4c98eb4dec55ac8e76cfe
data-pup <kevinmartinucr@gmail.com>
2018-02-17 09:26:23 -0800

    Added placeholder scaffolds to the naive encoding and decoding table test files.
    Fixup commit, added tslint flags for unused variables.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/ConversionTableTests/NaiveDecodingTable.test.ts
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import {
+//     NaiveDecodingTable,
+// } from '../../src/NaiveDecoding/NaiveDecodingTable';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestNaiveDecodingTable {
+
+    @test public placeholderTest() {
+        assert.equal(1, 2, 'NaiveDecodingTable tests not implemented yet!');
+    }
+
+}
---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/ConversionTableTests/NaiveEncodingTable.test.ts
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import {
+//     NaiveEncodingTable,
+// } from '../../src/NaiveEncoding/NaiveEncodingTable';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestNaiveEncodingTable {
+
+    @test public placeholderTest() {
+        assert.equal(1, 2, 'NaiveEncodingTable tests not implemented yet!');
+    }
+
+}
