f54d22034e72214477e2c500233f5072641c1740
data-pup <kevinmartinucr@gmail.com>
2018-02-18 08:59:35 -0800

    Added some notes to the table printing file. Still no implementation.

---
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/PrintingUtilities/printTables.ts
@@ -0,0 +1,7 @@
+// import {  } from 'module';
+// import {  } from '../';
+
+// TODO: Accept a table as an input,
+// find the table width & print each element aligned to this width.
+// TODO: Merge the table interfaces so that both huffman and naive
+// tables implement the same interface.
