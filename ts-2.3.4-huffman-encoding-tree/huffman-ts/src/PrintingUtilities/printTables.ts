820ee7d6b5637e552663ecea0f54ef59a13c9d9b
data-pup <kevinmartinucr@gmail.com>
2018-02-18 09:50:14 -0800

    Added some basic work on the table printing function. There is now a helper class for storing information about how to print the table. I am thinking about dividing the printing functions for the huffman tree, and for the encoding/decoding tables, into two separate directories, since these files are each a little bit large, and are likely to gain from some modularization so that I can test things better. As another side note, worth considering making these return strings, so that I can test their results a little easier.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/PrintingUtilities/printTables.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/src/PrintingUtilities/printTables.ts
@@ -1,12 +1,58 @@
+import { isNullOrUndefined } from 'util';
 import { IDecodingTable } from '../TableInterfaces/IDecodingTable';
 import { IEncodingTable } from '../TableInterfaces/IEncodingTable';
 
 // Print an object that implements the decoding table interface.
-const printEncodingTable = (table:IEncodingTable) : void => {
+export const printEncodingTable = (table:IEncodingTable) : void => {
     throw new Error('Not Implemented Yet!');
 };
 
 // Print an object that implements the decoding table interface.
-const printDecodingTable = (table:IDecodingTable) : void => {
+export const printDecodingTable = (table:IDecodingTable) : void => {
     throw new Error('Not Implemented Yet!');
 };
+
+// Print the header for an encoding table.
+const printEncodingTableHeader = () : void => {
+    throw new Error('Not Implemented Yet!');
+};
+
+// Print the header for an encoding table.
+const printDecodingTableHeader = () : void => {
+    throw new Error('Not Implemented Yet!');
+};
+
+class TableColumnPrintingInformation {
+
+    public columns:ColumnInfo[];
+
+    constructor(columns:ColumnInfo[]) {
+        this.columns = columns;
+        if (!this.validate()) {
+            throw new Error('Error initializing table printing information');
+        }
+    }
+
+    private validate() : boolean {
+        if (isNullOrUndefined(this.columns)) { return false; }
+        const noColumnsFailedBasicValidation = this.columns
+            .map((currColumn) : boolean => { // Check if the column passes
+                const colName:string = currColumn.name; // a basic width check.
+                const colWidth:number = currColumn.width;
+                const widthSeemsValid:boolean = (colName.length < colWidth);
+                return widthSeemsValid;
+            }) // Check that all of the columns seemed valid.
+            .every((value) => { return (value === true) });
+        return noColumnsFailedBasicValidation;
+    }
+}
+
+class ColumnInfo {
+    public readonly name:string;
+    public readonly width:number;
+
+    constructor(columnName:string, columnWidth:number) {
+        this.name = columnName;
+        this.width = columnWidth;
+    }
+}
