c996f349ff30f16da56ee88981442c9f79184fe7
data-pup <kevinmartinucr@gmail.com>
2018-02-17 17:59:17 -0800

    Added a simple test for the naive encoding and decoding table.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/test/ConversionTableTests/NaiveDecodingTable.test.ts
+++ ts-2.3.4-huffman-encoding-tree/huffman-ts/test/ConversionTableTests/NaiveDecodingTable.test.ts
@@ -21,4 +21,18 @@ import {
         assert.isTrue(ndt.decodingScheme.isEmpty());
     }
 
+    @test public testSimpleStringCanBeEncodedAndDecodedSuccessfully() {
+        // Declare a test string that will be encoded and then decoded.
+        const inputString = 'hello world';
+
+        // Create a naive encoding table and a naive decoding table.
+        const net = new NaiveEncodingTable(inputString);
+        const ndt = new NaiveDecodingTable(net);
+
+        // Encode the string, decode the result, and assert that the
+        // resulting plaintext matches the original string.
+        const outputString = ndt.decode(net.encode(inputString));
+        assert.equal(inputString, outputString);
+    }
+
 }
