0e935d1e932c7ba85cae52c2b65b9ebdc7b91376
data-pup <kevinmartinucr@gmail.com>
2018-02-20 20:35:26 -0800

    Removed unused classes from the conversion table printing directory.

--- ts-2.3.4-huffman-encoding-tree/huffman-ts/src/ConversionTables/Printing/ColumnPrintingInformation.ts
+++
@@ -1,9 +0,0 @@
-export class ColumnPrintingInformation {
-    public readonly name:string;
-    public readonly width:number;
-
-    constructor(columnName:string, columnWidth:number) {
-        this.name = columnName;
-        this.width = columnWidth;
-    }
-}
